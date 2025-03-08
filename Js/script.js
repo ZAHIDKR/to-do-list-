43f08cde633e112d1f71fbf3bb44fd963b7b81c2
ZAHIDKR <192165778+ZAHIDKR@users.noreply.github.com>
2025-03-08 10:08:38 +0530

    img change

--- Js/script.js
+++ Js/script.js
@@ -25,13 +25,13 @@ let currentFontSize = 100;
 
 // 🌟 Array of searchable names
 const people = [
-    { name: "boss", image: "/ai-exersice/appv6/assets/afiya.jpeg", desc: "A true leader and the gangstar!" },
-    { name: "afiya", image: "/ai-exersice/appv6/assets/afia.jpeg", desc: "A beautiful soul with a kind heart!" },
-    { name: "zihad", image: "/ai-exersice/appv6/assets/zihad.jpeg", desc: "Ami osho hai amake keo adopt koro!" },
-    { name: "arbind", image: "/ai-exersice/appv6/assets/arbind.jpeg", desc: "mai garib hu. koi bhi kam ho bas call lagao 100 rupiya mai sab kar dungga khana pakana bartan dhona !" },
-    { name: "piyush", image: "/ai-exersice/appv6/assets/piyush .jpeg", desc: "mai kho geya hu muje koi ghar le jao !" },
-    { name: "shihab", image: "/ai-exersice/appv6/assets/shihab.jpeg", desc: "ami akta valo shale keo pem korta chaile call dao !" },
-    { name: "afiya malek", image: "/ai-exersice/appv6/assets/afia.jpeg", desc: "A beautiful soul with a kind heart!" }
+    { name: "boss", image: "/to-do-list-/assets/afiya.jpeg", desc: "A true leader and the gangstar!" },
+    { name: "afiya", image: "/to-do-list-/assets/afia.jpeg", desc: "A beautiful soul with a kind heart!" },
+    { name: "zihad", image: "/to-do-list-/assets/zihad.jpeg", desc: "Ami osho hai amake keo adopt koro!" },
+    { name: "arbind", image: "/to-do-list-/assets/arbind.jpeg", desc: "mai garib hu. koi bhi kam ho bas call lagao 100 rupiya mai sab kar dungga khana pakana bartan dhona !" },
+    { name: "piyush", image: "/to-do-list-/assets/piyush .jpeg", desc: "mai kho geya hu muje koi ghar le jao !" },
+    { name: "shihab", image: "/to-do-list-/assets/shihab.jpeg", desc: "ami akta valo shale keo pem korta chaile call dao !" },
+    { name: "afiya malek", image: "/to-do-list-/assets/afia.jpeg", desc: "A beautiful soul with a kind heart!" }
 ];
 
 // Add Item to To-Do List
